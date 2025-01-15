adc00abf3184248c92bd5e76997627378aa56304
adekunleaishat1 <aishatadekunle877@gmail.com>
2025-01-15 13:44:52 +0100

    ternary operator

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,8 +2,13 @@ import { useState } from "react"
 
 
 const Landing = () =>{
+    const [isLoading, setIsLoading] = useState(false)
     const [username, setusername] = useState("Tolani")
-    // let username = "Tolani"
+    const [input, setinput] = useState("")
+    const [input2, setinput2] = useState("")
+    const [userray, setuserray] = useState([])
+    let gender = true
+
     let userobj = {
         name:"Bola", class:"React"
     }
@@ -18,24 +23,38 @@ const Landing = () =>{
         setusername("Precious")
         // console.log(username);   
     }
+    const Savechanges = () =>{
+        let userobj = {
+            input,
+            input2
+        }
+        console.log(userobj);
+        setuserray([...userray ,userobj])
+        console.log(userray);
+        setinput("")
+        setinput2("")
+    }
+    const handleInputType = () =>{
+        setIsLoading(isLoading ? false : true)
+    }
+    // const getinptchange = (l) =>{
+    //     alert(l)
+        
+    // //   console.log(e.target.value);
+    // //   setinput(e.target.value)
+      
+    // }
+    // {conditional epression? truth expression : false expression}
     return(
         <>
         <h1>Landing page</h1>
-        <p>{username}</p>
+        <p className={gender ? "text-primary": "text-success"}>{username}</p>
         <button onClick={click}>Click</button>
         <p>Name:{userobj.name } Class: {userobj.class}</p>
-        {/* <p>{userarray[0].name} {userarray[0].class}</p> */}
-        {/* {
-            userarray.map((user)=>{
-                return(
-                    <>
-                    <h1>{user.name}</h1>
-                    <h1>{user.class}</h1>
-                   </>
-                )
-               
-            })
-        } */}
+        <input value={input} onChange={(e)=>setinput(e.target.value)} type="text" />
+        <input value={input2} onChange={(e)=>setinput2(e.target.value)} type="text" />
+        <button onClick={Savechanges}>Submit</button>
+        <p>{userobj.input}</p>
         
         <table>
           <thead>
@@ -44,13 +63,13 @@ const Landing = () =>{
                 <th>Class</th>
             </tr>
           </thead>
-          {
-            userarray.map((user)=>(
+          {userray.length == 0 ? "user is not available" :
+            userray.map((user)=>(
                     <>
                         <tbody>
                             <tr>
-                                <td>{user.name}</td>
-                                <td>{user.class}</td>
+                                <td>{user.input}</td>
+                                <td>{user.input2}</td>
                             </tr>
                         </tbody>
                     
@@ -60,6 +79,9 @@ const Landing = () =>{
         }
         </table>
         
+
+        <input type={isLoading? "text" :  "password"} />
+        <button onClick={handleInputType}>Change</button>
 
         </>
     )
